071b3b8316e7bc0ff02719c55913d2074483cf8f
Nafiou <nafiou94@gmail.com>
2021-04-19 15:42:56 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+import { Language } from 'src/_enums/language.enum';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  selectedLanguage: Language | undefined;
+  Language = Language;
   title = 'jool';
+  constructor(
+    private translateService: TranslateService
+  ){
+    this.translateService.init()
+  }
+  ngOnInit(): void {
+    this.selectedLanguage = <Language>this.translateService.currentLang;
+    this.translateService.onLangChange.subscribe(
+      (event: LangChangeEvent) => (this.selectedLanguage = <Language>event.lang)
+    );
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHelperService } from 'src/_services/translate-helper.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useClass: TranslateHelperService,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
